b135099d122d7f14d8b241ef69666eb478492cc0
modalexii <kyle@unsecu.re>
2014-09-07 00:23:09 -0400

    Add fetching single events, fix updating events

--- event_calendar/gcal_api.py
+++ event_calendar/gcal_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 
 class calendarOperation():
 	'''
@@ -43,8 +43,7 @@ class calendarOperation():
 
 	def new_event(self,event_data):
 		'''
-		Add new event(s) to the calendar. If stage_only = True, do
-		not execute	the transaction.
+		Add new event(s) to the calendar.
 		'''
 		self.staged_transaction = self.service.events().insert(
 			calendarId = self.gcal_id, 
@@ -53,19 +52,33 @@ class calendarOperation():
 
 	def update_event(self,event_data,event_id):
 		'''
-		update event(s) to the calendar. If stage_only = True, do
-		not execute	the transaction.
+		Update existing event(s) on the calendar.
 		'''
-		self.staged_transaction = self.service.events().insert(
+		event_data["sequence"] += 1
+		self.staged_transaction = self.service.events().update(
 			calendarId = self.gcal_id, 
 			eventId = event_id,
 			body = event_data,
 		)
 
+	def get_event_by_id(self,event_id):
+		'''
+		Return the event object with the given ID 
+		'''
+		self.staged_transaction = self.service.events().get(
+			calendarId = self.gcal_id, 
+			eventId = event_id,
+		)
+
 	def list_future_events(self):
+		from datetime import datetime
+		today = datetime.now()
+		today = today.strftime("%Y-%m-%dT00:00:00+04:00")
+		# TODAY will be off by an hour during EDT. NBD.
+
 		self.staged_transaction = self.service.events().list(
 			calendarId = self.gcal_id, 
-			#timeMin = now, # by default only shows future events
+			timeMin = today,
 			showDeleted = True,
 		)
 		
